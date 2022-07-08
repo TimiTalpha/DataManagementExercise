cc544b700ffe455e914d4d8d6649dc81cd7ee00b
TimiTalpha <s1290237@u-aizu.ac.jp>
2022-07-08 13:50:51 +0900

    HoT: initial revision

---
+++ ex4.py
@@ -0,0 +1,22 @@
+import random
+
+def main():
+  cnt_h = 0
+  cnt_t = 0
+
+  print('Tossing a coin...')
+  for i in range(3):
+    number = random.randint(0, 4) % 2
+    value = 'Heads' if number == 0 else 'Tails'
+    if number == 0: cnt_h += 1
+    else: cnt_t += 1
+    print('Round {}, {}'.format(i+1, value))
+
+  print('Heads: {}, Tails: {}'.format(cnt_h, cnt_t))
+
+  return
+
+
+
+if __name__ == '__main__':
+  main()
